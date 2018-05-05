572749446068973dfe788576143305bac1d105a3
ramiro16 <ramirotapia16@gmail.com>
2018-05-05 00:21:55 -0300

    Add files via upload

--- Clase 10 Modelo Parcial/main.c
+++ Clase 10 Modelo Parcial/main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <conio.h>
 
 #define OCUPADO 0
 #define LIBRE 1
 
 #define TamU 5
+#define TamP 10
 
 typedef struct
 {
@@ -18,28 +20,56 @@ typedef struct
 
 }eUsuario;
 
+typedef struct
+{
+    int idProducto;
+    int idUsuario;
+    int stock;
+    float precio;
+    char nombre[50];
+
+    int estado;
+
+}eProducto;
+
 void hardcodearUsuarios(eUsuario[],int);
+void hardcodearProductos(eProducto[],int);
+
 void mostrarUsuarios(eUsuario[],int);
+void mostrarProductos(eProducto[],int);
+
+void eUsuario_mostrarUno(eUsuario);
 
 int eUsuario_init(eUsuario[],int);
+int eProducto_init(eProducto[],int);
 
-int eUsuario_alta(eUsuario[] ,int);
-int eUsuario_modificacion(eUsuario[] ,int);
+int eUsuario_alta(eUsuario[],int);
+int eUsuario_modificacion(eUsuario[],int);
+int eUsuario_baja(eUsuario[] ,int);
 
-int eUsuario_buscarLugarLibre(eUsuario listado[],int);
+int eProducto_alta(eProducto[],int,eUsuario[],int);
+
+int eUsuario_buscarLugarLibre(eUsuario[],int);
 int eUsuario_siguienteId(eUsuario[] ,int);
+
+int eProducto_buscarLugarLibre(eProducto[],int);
+int eProducto_siguienteId(eProducto[],int);
+
+
 
 
 
 int main()
 {
     eUsuario usuario[TamU];
-    int auxUser;
+    eProducto producto[TamP];
+    //int auxUser;
 
     char seguir='s';
     int opcion=0;
 
     eUsuario_init(usuario,TamU);
+    eProducto_init(producto,TamP);
 
     while(seguir=='s')
     {
@@ -64,7 +94,7 @@ int main()
             case 1:
                 system("cls");
                 hardcodearUsuarios(usuario,TamU);
-                //auxUser = eUsuario_alta(usuario,TamU);
+                eUsuario_alta(usuario,TamU);
                 system("pause");
                 system("cls");
                 break;
@@ -75,8 +105,18 @@ int main()
                 system("cls");
                 break;
             case 3:
+                system("cls");
+                eUsuario_baja(usuario,TamU);
+                system("pause");
+                system("cls");
                 break;
             case 4:
+                system("cls");
+                hardcodearProductos(producto,TamP);
+
+                eProducto_alta(producto,TamP,usuario,TamU);
+                system("pause");
+                system("cls");
                 break;
             case 5:
                 break;
@@ -87,10 +127,16 @@ int main()
             case 8:
                 break;
             case 9:
+                system("cls");
+                mostrarProductos(producto,TamP);
+                printf("\n");
+                system("pause");
+                system("cls");
                 break;
             case 10:
                 system("cls");
                 mostrarUsuarios(usuario,TamU);
+                printf("\n");
                 system("pause");
                 system("cls");
                 break;
@@ -140,7 +186,7 @@ void mostrarUsuarios(eUsuario usuario[],int tam)
 int eUsuario_alta(eUsuario listado[],int limite)
 {
     int retorno = -1; //DEVUELVE -1 SI LIMITE < 0
-    char nombre[50];
+    //char nombre[50];
     int id;
     int indice;
 
@@ -242,11 +288,11 @@ int eUsuario_modificacion(eUsuario listado[] ,int limite)
 
     if(limite > 0 && listado != NULL)
     {
-        retorno = -2; //Devuelve 0 si lograr pedir un ID a modificar pero no puede modificar nada
+        retorno = -2; //Devuelve -2 si lograr pedir un ID a modificar pero no puede modificar nada
 
         mostrarUsuarios(listado, limite);
 
-        printf("Ingrese ID de usuario a modificar:");
+        printf("\nIngrese ID de usuario a modificar:");
         scanf("%d",&aux);
 
         for(i=0;i<limite;i++)
@@ -255,7 +301,7 @@ int eUsuario_modificacion(eUsuario listado[] ,int limite)
             {
                 while(seguir == 's')
                 {
-                    printf("Que desea modificar?\n\n");
+                    printf("\nQue desea modificar?\n\n");
                     printf("1-Nombre\n");
                     printf("2-Edad\n");
 
@@ -264,29 +310,217 @@ int eUsuario_modificacion(eUsuario listado[] ,int limite)
                     switch(opcion)
                     {
                         case 1:
-                            printf("Ingrese nuevo nombre:");
+                            printf("\nIngrese nuevo nombre:");
                             fflush(stdin);
                             gets(listado[i].nombre);
                             retorno = 0;
                             seguir = 'n';
                             break;
                         case 2:
-                            printf("Ingrese nueva edad:");
-                            scanf("%d",listado[i].edad);
+                            printf("\nIngrese nueva edad:");
+                            scanf("%d",&listado[i].edad);
                             retorno = 0;
                             seguir = 'n';
                             break;
                     }
-
                 }
-
 
                 break;
             }
         }
-
     }
 
 
     return retorno;
+}
+
+int eUsuario_baja(eUsuario listado[] ,int limite)
+{
+    int retorno = -1; //NO LOGRA PASAR EL PRIMER IF
+    int aux;
+    int i;
+    char rta;
+
+    if(limite > 0 && listado != NULL)
+    {
+        retorno = -2; //SI EL ID INGRESADO ES INCORRECTO
+
+        mostrarUsuarios(listado, limite);
+
+        printf("\nIngrese ID de usuario que desea dar de baja:");
+        scanf("%d",&aux);
+
+        for(i=0;i<limite;i++)
+        {
+            if(listado[i].idUsuario == aux)
+            {
+                system("cls");
+                printf("Desea dar de baja al siguiente usuario? S/N\n\n");
+                eUsuario_mostrarUno(listado[i]);
+                rta = getch();
+
+                if(rta == 'S' || rta == 's')
+                {
+                    listado[i].estado = LIBRE;
+                    retorno = 0;
+                    printf("\nSE HA DADO DE BAJA CON EXITO!\n");
+                    break;
+                }
+                else
+                {
+                    printf("\nSE HA CANCELADO LA BAJA\n");
+                    break;
+                }
+
+            }
+        }
+    }
+
+    return retorno;
+}
+
+void eUsuario_mostrarUno(eUsuario listado)
+{
+    printf("ID USUARIO\tNOMBRE\t\tEDAD\n\n");
+    printf("%d%19s%13d\n",listado.idUsuario,listado.nombre,listado.edad);
+}
+
+int eProducto_init(eProducto listado[],int limite)
+{
+    int retorno = -1;
+    int i;
+    if(limite > 0 && listado != NULL)
+    {
+        retorno = 0;
+        for(i=0; i<limite; i++)
+        {
+            listado[i].estado = LIBRE;
+            listado[i].idProducto = 0;
+        }
+    }
+    return retorno;
+}
+
+void hardcodearProductos(eProducto listado[],int limite)
+{
+    int i;
+
+    int id[]={1000,1001,1002,1003,1004,1005,1006,1007,1008,1009};
+    int usuarioVendedor[]={100,104,102,100,101,103,104,100,102,104};
+    int stock[]={10,7,8,3,9,23,42,17,25,5};
+    float precio[]={10.20,7,25,34,10,25.50,120.30,9.99,12,300.35};
+    char nombre[][50]={"Joystick","Pelota Playa","Taza","Videojuego","Camiseta","Pintura","Carpeta","Paragua","Adorno","Trofeo"};
+
+    for(i=0;i<limite;i++)
+    {
+        listado[i].idProducto = id[i];
+        listado[i].stock = stock[i];
+        listado[i].precio = precio[i];
+        listado[i].idUsuario = usuarioVendedor[i];
+        strcpy(listado[i].nombre,nombre[i]);
+        listado[i].estado = OCUPADO;
+    }
+}
+
+void mostrarProductos(eProducto listado[],int limite)
+{
+    int i;
+
+    printf("ID PRODUCTO\tNOMBRE\t\tPRECIO\t\tSTOCK\n\n");
+
+    for(i=0;i<limite;i++)
+    {
+        if(listado[i].estado == 0)
+        {
+            printf("%d%19s%15.2f%14d\n",listado[i].idProducto,listado[i].nombre,listado[i].precio,listado[i].stock);
+        }
+    }
+}
+
+int eProducto_alta(eProducto listadoP[], int limiteP, eUsuario listadoU[], int limiteU)
+{
+    int i;
+
+    int retorno = -1; //DEVUELVE -1 SI LIMITE < 0
+    int auxId;
+    int indice;
+
+    if(limiteU > 0 && limiteP > 0 && listadoU != NULL && listadoP != NULL)
+    {
+        retorno = -2; //DEVUELVE -2 SI NO ENCUENTRA LUGAR LIBRE
+        mostrarUsuarios(listadoU,limiteU);
+        printf("Ingrese ID de usuario que realizara publicacion:");
+        scanf("%d",&auxId);
+
+        for(i=0;i<limiteU;i++)
+        {
+            if(listadoU[i].idUsuario == auxId)
+            {
+                indice = eProducto_buscarLugarLibre(listadoP,limiteP); //BUSCA LUGAR EN ARRAY PRODCUTO IGNORAR IDENTIFICADOR
+
+                if(indice >= 0)
+                {
+                    retorno = 0;
+
+                    auxId = eProducto_siguienteId(listadoP,limiteP);//GENERA UNA ID IGNORAR IDENTIFICADOR
+
+                    printf("Ingrese nombre de producto:");
+                    fflush(stdin);
+                    gets(listadoP[indice].nombre);
+
+                    printf("Ingrese cantidad de producto:");
+                    scanf("%d",&listadoP[indice].stock);
+
+                    printf("Ingrese precio de producto:");
+                    scanf("%f",&listadoP[indice].precio);
+
+                    listadoP[indice].idProducto = auxId;
+                    listadoP[indice].estado = OCUPADO;
+                }
+            }
+        }
+    }
+
+    return retorno;
+}
+
+int eProducto_buscarLugarLibre(eProducto listado[],int limite)
+{
+    int retorno = -1;
+    int i;
+    if(limite > 0 && listado != NULL)
+    {
+        retorno = -2;
+        for(i=0;i<limite;i++)
+        {
+            if(listado[i].estado == LIBRE)
+            {
+                retorno = i;
+                break;
+            }
+        }
+    }
+    return retorno;
+}
+
+int eProducto_siguienteId(eProducto listado[],int limite)
+{
+    int retorno = 0;
+    int i;
+    if(limite > 0 && listado != NULL)
+    {
+        for(i=0; i<limite; i++)
+        {
+            if(listado[i].estado == OCUPADO)
+            {
+                    if(listado[i].idUsuario>retorno)
+                    {
+                         retorno=listado[i].idUsuario;
+                    }
+            }
+        }
+    }
+
+    return retorno+1;
+
 }
